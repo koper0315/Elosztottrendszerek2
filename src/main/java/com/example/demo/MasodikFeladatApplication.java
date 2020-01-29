1108a9fd53b633c3366f7a56eea820a770d72df7
Péter Kovács <koper0315@gmail.com>
2020-01-29 13:36:11 +0100

    elso

---
+++ src/main/java/com/example/demo/MasodikFeladatApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasodikFeladatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasodikFeladatApplication.class, args);
+	}
+
+}
